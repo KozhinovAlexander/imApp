bae4be68847cb873b23ae73a8622249f6880f02e
Alexander Kozhinov <AlexanderKozhinov@yandex.com>
2020-05-28 11:13:37 +0200

    imgApp: mainwindow: layout:
    implemented main window layout
    
    Signed-off-by: Alexander Kozhinov <AlexanderKozhinov@yandex.com>

--- imApp/imApp_en_DE.ts
+++ imApp/imApp_en_DE.ts
@@ -5,7 +5,27 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <source>MainWindow</source>
+        <source>imApp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="57"/>
+        <source>Define Image Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <source>Start / Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="83"/>
+        <source>Processing Mode:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="42"/>
+        <source>Your image here!</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
